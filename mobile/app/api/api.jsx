e990b1317291c86b67535968d7bb7b6c56d8ab0b
Eike Frota <eikefrotaa@hotmail.com>
2025-10-20 20:04:40 -0300

    Subindo testes

--- mobile/app/api/api.jsx
+++ mobile/app/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.0.30.103:3000/api", // IP do servidor
+  baseURL: "http://10.0.30.56:3000/api", // IP do servidor
 });
 
 export default api;
